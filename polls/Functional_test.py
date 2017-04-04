6d866273fe8b56444e0b6665e6449bf51462d36e
Jairo Bernal <ji.bernal27@uniandes.edu.co>
2017-04-03 20:20:32 -0500

    mensaje

---
+++ polls/Functional_test.py
@@ -0,0 +1,15 @@
+__author__ = 'asistente'
+from unittest import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class FunctionalTest(TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(2)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    
